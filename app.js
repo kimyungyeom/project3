e01efdd4942303be18f192f5309f978feafbd9e4
kimyungyeom <kgk1599@gmail.com>
2023-11-15 09:03:33 +0900

    mov : 이전 입문주차에서 진행했던 프로젝트 코드들 이동 및 패키지설치, 포트번호숨기기

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 const express = require("express");
 // Express 애플리케이션 생성 / express() 함수 호출
 const app = express();
-// 포트 번호 설정
-const port = 3000;
+// 포트 번호 env 불러오기
+require('dotenv').config();
+const port = process.env.Server_port;
 // products.router.js 가져오기
 const productsRouter = require("./routes/products.router.js");
 // schemas 모듈들 가져오기
@@ -17,5 +18,5 @@ app.use("/api", [productsRouter]);
 
 // 서버 구동
 app.listen(port, () => {
-    console.log(port, "포트로 서버 구동을 시작합니다.");
+    console.log("서버 구동을 시작합니다.");
 })
