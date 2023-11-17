e4db77208ec1a2bedc80fa007b26cd5aa10fc458
kimyungyeom <kgk1599@gmail.com>
2023-11-17 10:43:51 +0900

    fix : user모델 보기편하게 코드들 정리

--- models/user.js
+++ models/user.js
@@ -1,29 +1,22 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+const { Model, DataTypes} = require("sequelize");
+
+module.exports = class User extends Model {
+    static init(sequelize) {
+        return User.init({
+            userId: {
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            email: DataTypes.STRING,
+            nickname: DataTypes.STRING,
+            password: DataTypes.STRING
+        }, {
+            sequelize,
+            modelName: 'User',
+        });
     }
-  };
-  User.init({
-    userId: {
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    email: DataTypes.STRING,
-    nickname: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+
+    static associate(models) {
+        // define association here
+    }
+}
