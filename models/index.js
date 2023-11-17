8e0d4c08432b2602eeb9a35fb4f01cd8dcac68b3
kimyungyeom <kgk1599@gmail.com>
2023-11-17 11:36:33 +0900

    fix : 코드에서 알 수 없는 부분을 제거하고 sequelize 유저, 상품모델 파일을 설정

--- models/index.js
+++ models/index.js
@@ -1,43 +1,23 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
+// sequelize모듈 가져오기
 const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
+// NODE_ENV 환경변수를 가져오는데 값이 존재하지 않다면 development를 기본값으로 사용
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+// config.js 가져오기 
+const config = require('../config/config.js')[env];
+
+// 유저 및 상품 모델 불러오기
+const User = require('./user');
+const Products = require('./products.schema.js');
+
+// 객체 생성 및 설정
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
+// 유저 및 상품의 Sequelize 모델 초기화
+User.init(sequelize);
+Products.init(sequelize);
+
+// db모듈 내보내기
 module.exports = db;
