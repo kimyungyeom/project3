75ab77a450c2685b73e4d98d6042acd5f10cabd7
kimyungyeom <kgk1599@gmail.com>
2023-11-17 11:35:17 +0900

    fix : mongo데이터 대신에 Sequelize 데이터를 이용하기 때문에 수정

--- models/products.schema.js
+++ models/products.schema.js
@@ -1,42 +1,53 @@
-// mongoose 모듈 가져오기
-const mongoose = require("mongoose");
+const { Model, DataTypes} = require("sequelize");
 
-const productsSchema = new mongoose.Schema({
-    // 상품명
-    productsName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    // 작성내용
-    contentWriting: {
-        type: String,
-        required: true
-    },
-    // 작성자명
-    name: {
-        type: String,
-        required: true
-    },
-    // 비밀번호
-    pw: {
-        type: Number,
-        required: true
-    },
-    // 상품상태
-    productStatus: {
-        type: String,
-        // 필드값 제한하기
-        enum: ["FOR_SALE", "SOLD_OUT"],
-        // 기본값 설정
-        default: "FOR_SALE"
-    },
-    // 작성날짜
-    date: {
-        type: Date,
-        default: Date.now
+module.exports = class Product extends Model {
+    static init(sequelize) {
+        return super.init({
+            // 유저ID
+            userId: {
+                type: DataTypes.INTEGER,
+            },
+            // 상품ID
+            productsId: {
+                primaryKey: true,
+                type : DataTypes.INTEGER
+            },
+            // 상품명
+            productsName: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            // 작성내용
+            contentWriting: {
+                type: String,
+                required: true
+            },
+            // 작성자명
+            name: {
+                type: String,
+                required: true
+            },
+            // 상품상태
+            productStatus: {
+                type: String,
+                // 필드값 제한하기
+                enum: ["FOR_SALE", "SOLD_OUT"],
+                // 기본값 설정
+                default: "FOR_SALE"
+            },
+            // 작성날짜
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }, {
+            sequelize,
+            modelName: 'Product',
+        });
     }
-});
 
-// products명으로 입력받은 데이터들을 MongoDB로 내보내기
-module.exports = mongoose.model("products", productsSchema);
+    static associate(models) {
+        // define association here
+    }
+}
