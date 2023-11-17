82718c8cf8087456f1ebba29db5fdf05aff4872b
kimyungyeom <kgk1599@gmail.com>
2023-11-17 10:58:04 +0900

    fix : 코드 원상복구

--- models/user.js
+++ models/user.js
@@ -1,22 +1,24 @@
-const { Model, DataTypes} = require("sequelize");
-
-module.exports = class User extends Model {
-    static init(sequelize) {
-        return User.init({
-            userId: {
-                primaryKey: true,
-                type: DataTypes.INTEGER,
-            },
-            email: DataTypes.STRING,
-            nickname: DataTypes.STRING,
-            password: DataTypes.STRING
-        }, {
-            sequelize,
-            modelName: 'User',
-        });
-    }
-
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
     static associate(models) {
-        // define association here
+      // define association here
     }
-}
+  };
+  User.init({
+    userId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    email: DataTypes.STRING,
+    nickname: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
