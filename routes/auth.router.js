467f4ae3d7ab14984775621c0107735795006e0b
kimyungyeom <kgk1599@gmail.com>
2023-11-17 14:33:34 +0900

    fix : 회원가입 API 저장 및 비밀번호 일치여부에서 compare메서드를 이용해 비교

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -4,9 +4,58 @@ const express = require("express");
 const router = express.Router();
 // jwt모듈 가져오기
 const jwt = require("jsonwebtoken");
+// bcrypt모듈 가져오기
+const bcrypt = require("bcrypt");
 
 const { Op } = require("sequelize");
 const { User } = require("../models");
+
+// 회원가입 API
+router.post("/users", async (req, res) => {
+    // 이메일, 닉네임, 비밀번호, 비밀번호확인을 데이터로 넘겨받음
+    const { email, nickname, password, confirmPassword } = req.body;
+
+    // 빈 곳 여부 체크
+    if (!email || !nickname || !password || !confirmPassword) {
+        return res.status(400).send({
+            errorMessage: "입력란 중 비어있는 곳이 있습니다.",
+         });
+    }
+
+    // 비밀번호 최소 6자 이상 및 서로 일치 여부 확인
+    if (password.length < 6 || password !== confirmPassword) {
+        return res.status(400).send({
+            errorMessage: "비밀번호 최소 6자 이상이어야 하며, 비밀번호가 서로 일치해야 합니다.",
+        });
+    }
+
+    // 이메일 형식 체크
+    const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).send({
+            errorMessage: "올바른 이메일 형식이 아닙니다.",
+        });
+    }
+
+    // email or nickname이 동일한게 이미 있는지 확인하기 위해 가져온다.
+    const existsUsers = await User.findAll({
+        where: {
+            [Op.or]: [{ email }, { nickname }],
+        },
+    });
+    if (existsUsers.length) {
+        return res.status(400).send({
+            errorMessage: "이메일 또는 닉네임이 이미 사용중입니다.",
+        });
+    }
+
+    // 비밀번호 hash화 시키기
+    const hashingPassword = bcrypt.hash(password, 10);
+  
+    // 이메일, 닉네임, 해시화한 비밀번호를 저장하고 회원가입 성공 시, 비밀번호를 제외 한 사용자 정보 반환.
+    await User.create({ email, nickname, hashingPassword });
+    res.status(201).json({ email, nickname });
+});
 
 // 로그인 API
 const secretKey = process.env.Secret_key;
@@ -22,12 +71,19 @@ router.post("/auth", async (req, res) => {
          },
     });
 
-    // 유저 존재 유무와 비밀번호 서로 일치여부 확인
-    if (!user || password !== user.password) {
-      res.status(400).send({
-         errorMessage: "이메일 또는 패스워드가 틀렸습니다.",
+    // 유저 존재 유무 확인
+    if (!user) {
+        return res.status(400).send({
+            errorMessage : "해당 이메일을 가진 유저가 존재하지 않습니다.",
+        });
+    }
+
+    // 비밀번호 서로 일치여부 확인
+    const match = await bcrypt.compare(password, user.hashingPassword);
+    if (!match) {
+      return res.status(400).send({
+         errorMessage: "비밀번호가 틀렸습니다. 다시 확인해주세요.",
       });
-      return;
     }
   
     // 로그인 성공 시 JWT AccessToken을 생성
@@ -42,7 +98,7 @@ router.post("/auth", async (req, res) => {
     // JWT를 Cookie로 할당
     res.cookie("Authorization", `Bearer ${accessToken}`);
     // 생성한 Token 반환
-    res.status(200).send({
+    return res.status(200).json({
         token: accessToken
     });
 });
