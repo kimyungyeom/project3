137939fdf567d9eb69ba8d1c0b4d0044c8952ead
kimyungyeom <kgk1599@gmail.com>
2023-11-20 09:34:16 +0900

    feat : 비밀번호를 제외한 내 정보 받아오기

--- routes/users.router.js
+++ routes/users.router.js
@@ -3,15 +3,18 @@ const express = require("express");
 // Express모듈에서 라우터 가져오기
 const router = express.Router();
 // 인증 middleware 가져오기
-const authMiddleware = require("../middlewares/need-signin.middleware");
+const authMiddleware = require("../middlewares/need-signin.middleware.js");
 
 // 내 정보 조회 API
 router.get("/user/me", authMiddleware, (req, res) => {
-    const { email, nickname } = res.locals.user;
+    // 비밀번호를 제외한 내 정보 받아오기
+    const { userId, email, nickname, createdAt, updatedAt } = res.locals.user
 
     // 비밀번호를 제외한 내 정보를 반환
     return res.status(200).json({
-        user : { email, nickname }
+        success: true,
+        message: "내 정보 조회가 완료되었습니다.",
+        user: { userId,  email, nickname, createdAt, updatedAt }
     });
 });
 
