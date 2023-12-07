269614945f0e578909aa0706b90f9641624dc8c2
kimyungyeom <kgk1599@gmail.com>
2023-12-08 01:23:07 +0900

    feat: DB 모델 수정
    - 사용자 migration 및 model 코드 수정
    - 상품 migration 및 model 코드 수정

--- models/products.cjs
+++ models/products.cjs
@@ -1,26 +1,55 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Products extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Products.init({
-    userId: DataTypes.INTEGER,
-    productName: DataTypes.STRING,
-    content: DataTypes.STRING,
-    status: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Products',
-  });
-  return Products;
+	class Products extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			this.belongsTo(models.Users, { as: "user" });
+		}
+	}
+	Products.init(
+		{
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+			},
+			userId: {
+				allowNull: false,
+				type: DataTypes.INTEGER,
+			},
+			productName: {
+				allowNull: false,
+				type: DataTypes.STRING,
+			},
+			content: {
+				allowNull: false,
+				type: DataTypes.STRING,
+			},
+			status: {
+				allowNull: false,
+				type: DataTypes.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Products",
+		},
+	);
+	return Products;
 };
--- models/users.cjs
+++ models/users.cjs
@@ -1,25 +1,51 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Users.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
-  return Users;
+	class Users extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			this.hasMany(models.Products, { foreignKey: "userId", as: "products" });
+		}
+	}
+	Users.init(
+		{
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+			},
+			email: {
+				allowNull: false,
+				type: DataTypes.STRING,
+			},
+			password: {
+				allowNull: false,
+				type: DataTypes.STRING,
+			},
+			name: {
+				allowNull: false,
+				type: DataTypes.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Users",
+		},
+	);
+	return Users;
 };
