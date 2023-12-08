7394dfbd6df2f377197aa13a9e0ed490775df079
kimyungyeom <kgk1599@gmail.com>
2023-12-08 12:09:59 +0900

    style: 예상치 못한 에러 발생한 경우에 대한 주석 추가

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -61,6 +61,8 @@ router.post("/signup", async (req, res) => {
 		});
 	} catch (err) {
 		console.error(err);
+
+		// 예상치 못한 에러 발생한 경우
 		return res.status(500).send({
 			success: false,
 			errorMessage: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해주세요.",
@@ -116,6 +118,8 @@ router.post("/login", async (req, res) => {
 		});
 	} catch (err) {
 		console.error(err);
+
+		// 예상치 못한 에러 발생한 경우
 		return res.status(500).send({
 			success: false,
 			errorMessage: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해주세요.",
