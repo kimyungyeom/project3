9aae4cff81b755075a4345ebd164afb474801e58
kimyungyeom <kgk1599@gmail.com>
2023-12-08 15:53:55 +0900

    feat: 상품 모델에서 상태 기본값이 빠져있던 부분 추가

--- models/products.cjs
+++ models/products.cjs
@@ -34,6 +34,7 @@ module.exports = (sequelize, DataTypes) => {
 			status: {
 				allowNull: false,
 				type: DataTypes.STRING,
+				defaultValue: "FOR_SALE",
 			},
 			createdAt: {
 				allowNull: false,
