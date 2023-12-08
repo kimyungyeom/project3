842e07ae4704597324f877a542ddc2655aa20faa
kimyungyeom <kgk1599@gmail.com>
2023-12-08 18:42:30 +0900

    feat, refactor: 상품 라우터 리팩토링 및 API 기능 구현, 테스트 진행
    - common js에서 ESM으로 코드 리팩토링
    - 상품 생성 API 기능 구현 및 테스트
    - 상품 목록 조회 API 기능 구현 및 테스트
    - 상품 상세 조회 API 기능 구현 및 테스트
    - 상품 수정 API 기능 구현 및 테스트
    - 상품 삭제 API 기능 구현 및 테스트

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,39 +1,216 @@
-// Express모듈 가져오기
-const express = require("express");
-// Express모듈에서 라우터 가져오기
+// import
+import express from "express";
+import Sequelize from "sequelize";
+import authMiddleware from "../middlewares/need-login.middleware.js";
+import db from "../models/index.cjs";
+
+// products.router.js - global variables
 const router = express.Router();
-// 인증 middleware 가져오기
-const authMiddleware = require("../middlewares/need-signin.middleware");
+const { Products, Users } = db;
 
 // 상품 생성 API
-// products.schema.js 가져오기
-const products = require("../models/products.schema.js");
+router.post("/", authMiddleware, async (req, res) => {
+	try {
+		const { id: userId } = res.locals.user;
+		const { productName, content } = req.body;
 
-router.post("/products", authMiddleware, async (req, res) => {
-	// 상품명, 작성내용을 req.body로 전달 받기
-	const { productsName, contentWriting } = req.body;
+		// 상품명이 없는 경우
+		if (!productName) {
+			return res.status(400).json({
+				success: false,
+				errorMessage: "상품명이 필요합니다.",
+			});
+		}
 
-	// 입력받은 데이터 및 기존 데이터를 이용해 생성한 값을 할당
-	const createdProducts = await products.create({ productsName, contentWriting });
+		// 상품 설명이 없는 경우
+		if (!content) {
+			return res.status(400).json({
+				success: false,
+				errorMessage: "상품 설명이 필요합니다.",
+			});
+		}
 
-	return res.status(201).json({
-		success: true,
-		message: "상품이 생성되었습니다.",
-		data: createdProducts,
-	});
+		const createdProduct = await Products.create({ productName, content, userId });
+
+		return res.status(201).json({
+			success: true,
+			message: "상품 생성이 완료되었습니다.",
+			data: { createdProduct },
+		});
+	} catch (err) {
+		return res.status(500).send({
+			success: false,
+			errorMessage: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해주세요.",
+		});
+	}
 });
 
 // 상품 목록 조회 API
-router.get("/products", async (req, res) => {});
+router.get("/", async (req, res) => {
+	try {
+		// QueryString을 이용한 정렬 방식
+		const { sort } = req.query;
+		let upperSort = sort?.toUpperCase();
+
+		// 상품 목록 정렬 - 최신순
+		if (upperSort !== "ASC" && upperSort !== "DESC") {
+			upperSort = "DESC";
+		}
+
+		const products = await Products.findAll({
+			attributes: ["id", "productName", "content", "status", "userId", [Sequelize.col("user.name"), "name"], "createdAt", "updatedAt"],
+			order: [["createdAt", upperSort]],
+			include: { model: Users, as: "user", attributes: [] },
+		});
+
+		return res.status(200).json({
+			success: true,
+			message: "상품 조회가 완료되었습니다.",
+			data: products,
+		});
+	} catch (err) {
+		return res.status(500).send({
+			success: false,
+			errorMessage: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해주세요.",
+		});
+	}
+});
 
 // 상품 상세 조회 API
-router.get("/products/:productsName", async (req, res) => {});
+router.get("/:productId", async (req, res) => {
+	try {
+		const { productId } = req.params;
+
+		const product = await Products.findOne({
+			where: { id: productId },
+			attributes: ["id", "productName", "content", "status", "userId", [Sequelize.col("user.name"), "name"], "createdAt", "updatedAt"],
+			include: { model: Users, as: "user", attributes: [] },
+		});
+
+		// 일치하는 상품이 없는 경우
+		if (!product) {
+			return res.status(404).send({
+				success: false,
+				errorMessage: "일치하는 상품이 존재하지 않습니다.",
+			});
+		}
+
+		return res.status(200).json({
+			success: true,
+			message: "상품 상세 조회가 완료되었습니다.",
+			data: product,
+		});
+	} catch (err) {
+		return res.status(500).send({
+			success: false,
+			errorMessage: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해주세요.",
+		});
+	}
+});
 
 // 상품 정보 수정 API
-router.put("/products/:productsName", authMiddleware, async (req, res) => {});
+router.put("/:productId", authMiddleware, async (req, res) => {
+	try {
+		const { productId } = req.params;
+		const { id: userId } = res.locals.user;
+		const { productName, content, status } = req.body;
+
+		// 수정 정보가 하나도 없는 경우
+		if (!productName && !content && !status) {
+			return res.status(400).send({
+				success: false,
+				errorMessage: "수정 정보는 최소 1가지 이상 입력해주세요.",
+			});
+		}
+
+		// 지원하지 않은 상태 값 입력된 경우
+		const isValidStatus = status ? status === "FOR_SALE" || status === "SOLD_OUT" : true;
+		if (!isValidStatus) {
+			return res.status(400).send({
+				success: false,
+				errorMessage: "지원하지 않는 상태 값 입니다. (status: FOR_SALE 또는 SOLD_OUT)",
+			});
+		}
+
+		const product = await Products.findOne({
+			where: { id: productId },
+		});
+
+		// 일치하는 상품이 없는 경우
+		if (!product) {
+			return res.status(404).send({
+				success: false,
+				errorMessage: "일치하는 상품이 존재하지 않습니다.",
+			});
+		}
+
+		// 작성자 ID와 인증 정보의 사용자 ID가 다른 경우
+		const isSameId = product.userId === userId;
+		if (!isSameId) {
+			return res.status(403).send({
+				success: false,
+				errorMessage: "권한이 없습니다.",
+			});
+		}
+
+		// 상품 수정
+		product.update({ productName, content, status }, { where: { productId } });
+
+		return res.status(200).json({
+			success: true,
+			message: "상품 수정이 완료되었습니다.",
+			data: product,
+		});
+	} catch (err) {
+		return res.status(500).send({
+			success: false,
+			errorMessage: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해주세요.",
+		});
+	}
+});
 
 // 상품 삭제 API
-router.delete("/products/:productsName", authMiddleware, async (req, res) => {});
+router.delete("/:productId", authMiddleware, async (req, res) => {
+	try {
+		const { productId } = req.params;
+		const { id: userId } = res.locals.user;
 
-// router 모듈 내보내기
-module.exports = router;
+		const product = await Products.findOne({
+			where: { id: productId },
+		});
+
+		// 일치하는 상품이 없는 경우
+		if (!product) {
+			return res.status(404).send({
+				success: false,
+				errorMessage: "일치하는 상품이 존재하지 않습니다.",
+			});
+		}
+
+		// 작성자 ID와 인증 정보의 사용자 ID가 다른 경우
+		const isSameId = product.userId === userId;
+		if (!isSameId) {
+			return res.status(403).send({
+				success: false,
+				errorMessage: "권한이 없습니다.",
+			});
+		}
+
+		// 상품 삭제
+		product.destroy({ where: { productId } });
+
+		return res.status(200).json({
+			success: true,
+			message: "상품 삭제가 완료되었습니다.",
+			data: product,
+		});
+	} catch (err) {
+		return res.status(500).send({
+			success: false,
+			errorMessage: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해주세요.",
+		});
+	}
+});
+
+// export
+export default router;
